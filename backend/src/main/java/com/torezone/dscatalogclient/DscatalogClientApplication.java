701293399304dac4ffcb54fa6b04e4618157069f
Jair Torezone <jairtorezone@gmail.com>
2022-08-20 21:02:09 -0300

    Backent project created

---
+++ backend/src/main/java/com/torezone/dscatalogclient/DscatalogClientApplication.java
@@ -0,0 +1,13 @@
+package com.torezone.dscatalogclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogClientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/torezone/dscatalogclient/DscatalogClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torezone.dscatalogclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

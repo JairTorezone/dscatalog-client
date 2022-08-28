a3b6ebb5c344f3f27a7f565340aa84e02097e2b3
Jair Torezone <jairtorezone@gmail.com>
2022-08-27 21:32:03 -0300

    Pagination

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -19,3 +19,38 @@ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Miguel 
 INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Arthur Torezone Rocha', '36621078056', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 2);
 INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Beijamin Costa Torres', '65301496000', 3275.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 2);
 
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Paulo Henrique Maximiano Santos', '63621078056', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 0);
+
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marcilene Rau Soares', '124621078056', 1240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marcio Rogerio da Silva', '697141496000', 3275.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 1);
+
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marta da Conceição', '29621078056', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 6);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Henlinson Rocha neves', '98721078056', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Antônia Terezine Ribeiro', '74901496057', 3275.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 7);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Thiago dos Santos Oliveira', '65301496041', 1475.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Severina Matildes  Dutra', '75421078056', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Sebastião Euzebio Oliveira', '14801496030', 2375.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 9);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Flavia Silva', '49721078019', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('André Campos Sales', '19721078047', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marina Marcedo Rocha', '01401496075', 3275.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 7);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Celeste Rochistova Marais', '12001496014', 1475.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Severino Querus Galhas', '12521078057', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Sebastiana Trindade Silva', '14780149603', 2375.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Fabiano Lines Maia', '12821078011', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Odete Conceição Ribeiro', '12521078056', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro Euzebio Conceição', '98801496000', 3275.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Sofia Neves dos Santos Oliveira', '19301496081', 1475.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ailton Rocha  Dutra', '01421078089', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Clenilde Oston Oliveira', '74801496047', 2375.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 9);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Eduarda Marta da Conceição', '29621078056', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 6);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joaquim Henlinson Rocha neves', '98721078056', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joyce Antônia Terezine Ribeiro', '74901496057', 3275.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 7);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marcus Vinicius dos Santos Oliveira', '65301496041', 1475.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Agata Severina Matildes  Dutra', '75421078056', 2240.55, TIMESTAMP WITH TIME ZONE '2020-01-01T10:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Tomas Sebastião Euzebio Oliveira', '14801496030', 2375.10, TIMESTAMP WITH TIME ZONE '2017-10-21T10:50:07.12345Z', 9);
